4e417eb78b19adda706ccc64a4e597abec1e2691
afx-nicolas <afxnicolas@gmail.com>
2022-05-28 15:44:51 -0300

    Add search movies

--- index.js
+++ index.js
@@ -1,67 +1,13 @@
-const movies = [
-  {
-    poster:
-      'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/4q2NNj4S5dG2RLF9CpXsej7yXl.jpg',
-    title: 'Spider-Man: Far From Home',
-    rating: 7.5,
-    release: '2019',
-    overview:
-      'Peter Parker and his friends go on a summer trip to Europe. However, they will hardly be able to rest - Peter will have to agree to help Nick Fury uncover the mystery of creatures that cause natural disasters and destruction throughout the continent.',
-  },
-  {
-    poster:
-      'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/6DrHO1jr3qVrViUO6s6kFiAGM7.jpg',
-    title: 'Sonic the Hedgehog 2',
-    rating: 7.8,
-    release: '2022',
-    overview:
-      'After settling in Green Hills, Sonic is eager to prove he has what it takes to be a true hero. His test comes when Dr. Robotnik returns, this time with a new partner, Knuckles, in search for an emerald that has the power to destroy civilizations. Sonic teams up with his own sidekick, Tails, and together they embark on a globe-trotting journey to find the emerald before it falls into the wrong hands.',
-  },
-  {
-    poster:
-      'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/teCy1egGQa0y8ULJvlrDHQKnxBL.jpg',
-    title: 'Hotel Transylvania: Transformania',
-    rating: 7.1,
-    release: '2022',
-    overview: `When Van Helsing's mysterious invention, the "Monsterfication Ray," goes haywire, Drac and his monster pals are all transformed into humans, and Johnny becomes a monster. In their new mismatched bodies, Drac and Johnny must team up and race across the globe to find a cure before it's too late, and before they drive each other crazy.`,
-  },
-  {
-    poster:
-      'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/eO0QV5qJaEngP1Ax9w3eV6bJG2f.jpg',
-    title: 'Halo',
-    rating: 8.6,
-    release: '2022',
-    overview:
-      'Depicting an epic 26th-century conflict between humanity and an alien threat known as the Covenant, the series weaves deeply drawn personal stories with action, adventure and a richly imagined vision of the future.',
-  },
-  {
-    poster:
-      'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/tlZpSxYuBRoVJBOpUrPdQe9FmFq.jpg',
-    title: 'Uncharted',
-    rating: 7.2,
-    release: '2022',
-    overview:
-      'A young street-smart, Nathan Drake and his wisecracking partner Victor “Sully” Sullivan embark on a dangerous pursuit of “the greatest treasure never found” while also tracking clues that may lead to Nathan’s long-lost brother.',
-  },
-  {
-    poster:
-      'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/4j0PNHkMr5ax3IA8tjtxcmPU3QT.jpg',
-    title: 'Encanto',
-    rating: 7.7,
-    release: '2021',
-    overview: `The tale of an extraordinary family, the Madrigals, who live hidden in the mountains of Colombia, in a magical house, in a vibrant town, in a wondrous, charmed place called an Encanto. The magic of the Encanto has blessed every child in the family with a unique gift from super strength to the power to heal—every child except one, Mirabel. But when she discovers that the magic surrounding the Encanto is in danger, Mirabel decides that she, the only ordinary Madrigal, might just be her exceptional family's last hope.`,
-  },
-  {
-    poster:
-      'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg',
-    title: 'Spider-Man: No Way Home',
-    rating: 8.1,
-    release: '2021',
-    overview:
-      'Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.',
-  },
-];
+import { API_KEY } from './env.js';
 
+const clientLanguage = RegExp(/([a-z]{2})-([A-Z]{2})/).test(navigator.language)
+  ? navigator.language
+  : 'en-US';
+
+const url = `https://api.themoviedb.org/3`;
+
+const searchForm = document.getElementById('search-bar');
+const searchInput = searchForm.elements['search-movie'];
 const moviesContainer = document.querySelector('section.movies-container');
 
 function createMovieOverview(overview) {
@@ -79,6 +25,7 @@ function createMovieOverview(overview) {
 function createFavoriteButton() {
   const buttonFavorite = document.createElement('button');
 
+  buttonFavorite.classList.add('favorite-btn');
   buttonFavorite.innerHTML = `
 		<svg width="23" height="21" viewBox="0 0 23 21" class="favorite-icon" fill="none"
 			xmlns="http://www.w3.org/2000/svg">
@@ -88,6 +35,10 @@ function createFavoriteButton() {
 		</svg>
 		Favoritar
 	`;
+
+  buttonFavorite.addEventListener('click', (e) => {
+    e.target.classList.toggle('active');
+  });
 
   return buttonFavorite;
 }
@@ -148,10 +99,16 @@ function createMovieImg(src, alt) {
   const divMovieImg = document.createElement('div');
   const img = document.createElement('img');
 
-  divMovieImg.classList.add('movie-img');
   img.src = src;
   img.alt = alt;
+
+  divMovieImg.classList.add('movie-img');
   divMovieImg.appendChild(img);
+
+  img.addEventListener('error', (e) => {
+    e.target.src = '/assets/image-not-found.svg';
+    e.target.classList.add('not-found');
+  });
 
   return divMovieImg;
 }
@@ -163,8 +120,21 @@ function createMovieItem() {
   return sectionMovieItem;
 }
 
-(function renderMovies() {
-  const moviesElementsList = movies.map(
+async function renderMovies(movies) {
+  console.log(movies);
+  const popularMovies = movies.map(
+    ({ poster_path, title, vote_average, release_date, overview }) => {
+      return {
+        poster: `https://image.tmdb.org/t/p/w300${poster_path}`,
+        title,
+        rating: vote_average.toFixed(1),
+        release: release_date.split('-')[0],
+        overview: overview,
+      };
+    }
+  );
+
+  const moviesElementsList = popularMovies.map(
     ({ poster, title, rating, release, overview }) => {
       const movieItem = createMovieItem();
       const movieImg = createMovieImg(poster, `${title} Poster`);
@@ -182,4 +152,54 @@ function createMovieItem() {
   moviesElementsList.forEach((movieElement) =>
     moviesContainer.appendChild(movieElement)
   );
+}
+
+async function searchMovie(query) {
+  moviesContainer.innerHTML = '<div class="loading"></div>';
+  const searchParams = `/search/movie?query=${query}&language=${clientLanguage}&include_adult=false&api_key=`;
+  const queryResults = await fetch(url + searchParams + API_KEY)
+    .then((response) => response.json())
+    .then((json) => json.results)
+    .then((movies) => movies)
+    .catch((error) => error);
+
+  moviesContainer.innerHTML = '';
+  renderMovies(queryResults);
+}
+
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const query = searchInput.value;
+  const isValid = query.replace(/\s/g, '').length > 4;
+  console.log(searchInput);
+
+  if (isValid) {
+    searchMovie(query);
+    return;
+  }
+
+  searchForm.classList.add('invalid');
+});
+
+searchInput.addEventListener('keyup', () => {
+  searchForm.classList.remove('invalid');
+});
+
+// Asynchronously get movies from the API
+(async function getPopularMovies() {
+  const movies = await fetch(
+    url + `/movie/popular?language=${clientLanguage}&api_key=` + API_KEY
+  )
+    .then((response) => response.json())
+    .then((json) => json.results)
+    .then((movies) => movies)
+    .catch((error) => console.error(error));
+
+  if (!movies) {
+    moviesContainer.innerHTML = '<h2>An error occurred, please try again.</h2>';
+    return;
+  }
+
+  moviesContainer.innerHTML = '';
+  renderMovies(movies);
 })();
